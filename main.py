6c228bcc1585fd86a38a9cd97414e60762e38dd5
Pawel Pelar <pawelpelar@gmail.com>
2023-05-24 12:12:18 +0200

    Main refactor

--- main.py
+++ main.py
@@ -9,47 +9,61 @@ from sampling import (
 from handle_pickle import save_pickle, load_pickle
 
 
+class MachineLearning:
+    def __init__(self):
+        self.reload_data = False
+        self.perform_sampling = False
+        self.functions = [
+            random_undersampling,
+            random_oversampling,
+            perform_smote,
+            perform_adasyn,
+        ]
+        self.function_names = [func.__name__ for func in self.functions]
+
+    def main(self):
+        dfs, sampled_dfs = self.load()
+        print(dfs)
+
+    def load(self):
+
+        cwd = os.getcwd()
+        df_path = os.path.join(cwd, "dataframes")
+        data_path = os.path.join(df_path, "data.pkl")
+        pre_sample, post_sample = "data_", ".pkl"
+
+        if self.reload_data:
+            raw_file_paths = get_paths()
+            dfs = load_files_into_dataframes(raw_file_paths)
+            save_pickle(dfs, data_path)
+        else:
+            dfs = load_pickle(data_path)
+
+        if self.perform_sampling:
+            sampled_dfs = []
+            for func in self.functions:
+                updated_structure = {}
+                for name, dataframes in dfs.items():
+                    try:
+                        updated_dataframes = [func(df, "Class") for df in dataframes]
+                        updated_structure[name] = updated_dataframes
+                    except Exception as err:
+                        print(err)
+                        print(name, func)
+                sampled_dfs.append(updated_structure)
+            for f_name, data in zip(self.function_names, sampled_dfs):
+                save_pickle(data, os.path.join(df_path, pre_sample + f_name + post_sample))
+        else:
+            sampled_dfs = []
+            for f_name in self.function_names:
+                sampled_dfs.append(
+                    load_pickle(os.path.join(df_path, pre_sample + f_name + post_sample))
+                )
+
+        return dfs, sampled_dfs
+
+
 if __name__ == "__main__":
-    reload_data = True
-    perform_sampling = True
 
-    functions = [
-        random_undersampling,
-        random_oversampling,
-        perform_smote,
-        perform_adasyn,
-    ]
-    function_names = [func.__name__ for func in functions]
-
-    cwd = os.getcwd()
-    df_path = os.path.join(cwd, "dataframes")
-    data_path = os.path.join(df_path, "data.pkl")
-    pre_sample, post_sample = "data_", ".pkl"
-
-    if reload_data:
-        raw_file_paths = get_paths()
-        dfs = load_files_into_dataframes(raw_file_paths)
-        save_pickle(dfs, data_path)
-    else:
-        dfs = load_pickle(data_path)
-
-    if perform_sampling:
-        sampled_dfs = []
-        for func in functions:
-            updated_structure = {}
-            for name, dataframes in dfs.items():
-                try:
-                    updated_dataframes = [func(df, "Class") for df in dataframes]
-                    updated_structure[name] = updated_dataframes
-                except Exception as err:
-                    print(err)
-                    print(name, func)
-            sampled_dfs.append(updated_structure)
-        for f_name, data in zip(function_names, sampled_dfs):
-            save_pickle(data, os.path.join(df_path, pre_sample + f_name + post_sample))
-    else:
-        sampled_dfs = []
-        for f_name in function_names:
-            sampled_dfs.append(
-                load_pickle(os.path.join(df_path, pre_sample + f_name + post_sample))
-            )
+    obj = MachineLearning()
+    obj.main()
