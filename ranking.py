b871782a99729d82c794572475c163d59a568f0b
Lukasbxd <lukas.babecki@gmail.com>
2023-06-20 23:19:17 +0200

    changed comments

--- ranking.py
+++ ranking.py
@@ -1,38 +1,38 @@
 import json
 import glob
 
-# Lista plików JSON
+# json list
 json_files = glob.glob('results/average_*.json')
 
-# Przechowywanie wyników
+# store results
 ranking_results = {}
 
-# Pętla przez wszystkie pliki JSON
+# looping through jsons
 for json_file in json_files:
     with open(json_file) as file:
         json_data = json.load(file)
         
-    # Przechodzenie przez wszystkie datasety w pliku JSON
+    # Iterate through all datasets in the JSON file
     for dataset_name, dataset in json_data.items():
-        # Pobieranie wyników dla klasyfikatorów
+        # Get results for classifiers
         random_forest_result = dataset['random_forest']['balanced_accuracy']
         decision_tree_result = dataset['decision_tree']['balanced_accuracy']
         naive_bayes_result = dataset['naive_bayes']['balanced_accuracy']
         
-        # Wykonanie testu rankingowego na podstawie wyników balanced_accuracy
+        # Perform ranking test based on balanced_accuracy results
         ranking = sorted([
             ('Random Forest', random_forest_result),
             ('Decision Tree', decision_tree_result),
             ('Naive Bayes', naive_bayes_result)
         ], key=lambda x: x[1], reverse=True)
         
-        # Dodanie wyników do słownika ranking_results
+        # Add results to ranking_results dictionary
         if dataset_name not in ranking_results:
             ranking_results[dataset_name] = []
         
         ranking_results[dataset_name].append(ranking)
         
-# Wyświetlanie wyników testu rankingowego dla każdego datasetu
+# Display ranking test results for each dataset
 for dataset_name, rankings in ranking_results.items():
     print(f"Dataset: {dataset_name}")
     for i, ranking in enumerate(rankings):
